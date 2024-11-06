1eb52b82dba8b08d464225147f181f9b50b830cd
Dimitar Dimitrov <dimitrov.d@northeastern.edu>
2024-11-05 19:59:58 -0500

    implement initial test

---
+++ math7243_xn2/initial_test.py
@@ -0,0 +1,115 @@
+# pylint:disable=invalid-name,fixme
+"""This script fits some basic models to CRISPR_gene_effect vs lineage"""
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+
+
+def get_data(y_name: str) -> Tuple[np.ndarray, np.ndarray]:
+    """Get X and y data"""
+
+    crispr_gene_effect = pd.read_csv("cache/22Q2/CRISPR_gene_effect.csv")
+    sample_info = pd.read_csv("cache/22Q2/sample_info.csv")
+
+    crispr_gene_effect_nona = crispr_gene_effect.dropna()
+    crispr_gene_effect_meta = pd.merge(
+        crispr_gene_effect_nona, sample_info, on="DepMap_ID", how="inner"
+    )
+
+    labels = []
+    indices = []
+    for _, row in crispr_gene_effect_meta.iterrows():
+        # TODO: don't iterate through rows
+        lineage = row[y_name]
+        if lineage not in labels:
+            labels.append(lineage)
+        indices.append(labels.index(lineage))
+
+    X_data = crispr_gene_effect_nona.select_dtypes(np.number).to_numpy()
+    y_data = np.array(indices)
+
+    return X_data, y_data
+
+
+def do_clf(
+    X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray
+) -> None:
+    """Perform Logistic Regression"""
+
+    clf = LogisticRegression(solver="lbfgs", max_iter=100)
+    clf.fit(X_train, y_train)
+    print(f"Logistic Regression Training Score: {clf.score(X_train, y_train):.3f}")
+    print(f"Logistic Regression Testing Score: {clf.score(X_test, y_test):.3f}")
+
+
+def do_lda(
+    X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray
+) -> None:
+    """Perform Linear Discriminant Analysis"""
+
+    lda = LinearDiscriminantAnalysis(store_covariance=True)
+    lda.fit(X_train, y_train)
+    print(f"LDA Training Score: {lda.score(X_train, y_train):.3f}")
+    print(f"LDA Testing Score: {lda.score(X_test, y_test):.3f}")
+
+
+def do_qda(
+    X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray
+) -> None:
+    """Perform Quadratic Discriminant Analysis"""
+
+    qda = QuadraticDiscriminantAnalysis(store_covariance=True)
+    qda.fit(X_train, y_train)
+    print(f"QDA Training Score: {qda.score(X_train, y_train):.3f}")
+    print(f"QDA Testing Score: {qda.score(X_test, y_test):.3f}")
+
+
+def do_lr(
+    X_train: np.ndarray,
+    y_train_dummy: np.ndarray,
+    X_test: np.ndarray,
+    y_test_dummy: np.ndarray,
+) -> None:
+    """Perform Linear Regression"""
+
+    lr = LinearRegression()
+    lr.fit(X_train, y_train_dummy)
+    print(f"R2 Training Score: {lr.score(X_train, y_train_dummy):.3f}")
+    print(f"R2 Testing Score: {lr.score(X_test, y_test_dummy):.3f}")
+
+
+def run_initial_test(y_name: str = "lineage", test_fraction: float = 1.0 / 7) -> None:
+    """Run an initial test"""
+
+    X_data, y_data = get_data(y_name)
+
+    test_count = round(len(y_data) * test_fraction)
+    X_train = X_data[test_count:]
+    y_train = y_data[test_count:]
+    X_test = X_data[:test_count]
+    y_test = y_data[:test_count]
+    y_train_dummy = pd.get_dummies(y_train).to_numpy()
+    y_test_dummy = pd.get_dummies(y_test).to_numpy()
+
+    print(f"Training Samples: {len(y_train)}")
+    print(f"Testing Samples: {len(y_test)}")
+
+    do_clf(X_train, y_train, X_test, y_test)
+    do_lda(X_train, y_train, X_test, y_test)
+    do_qda(X_train, y_train, X_test, y_test)
+    do_lr(X_train, y_train_dummy, X_test, y_test_dummy)
+
+
+def main() -> None:
+    """Entry Point"""
+
+    run_initial_test()
+
+
+if __name__ == "__main__":
+    main()
