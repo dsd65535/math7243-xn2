5155ce895a0f8439829d27c18aeedbbc3ba54113
Dimitar Dimitrov <dimitrov.d@northeastern.edu>
2024-12-09 00:55:36 -0500

    deprecate experimental scripts

--- math7243_xn2/__main__.py
+++ math7243_xn2/__main__.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
@@ -17,8 +18,6 @@ from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import SVC
-
-from math7243_xn2.basic import get_data
 
 
 def _get_accuracy_from_cm(cm: np.ndarray) -> float:
@@ -31,6 +30,40 @@ def _get_feature_count_from_coefs(coefs: np.ndarray) -> int:
     """Get the non-zero feature count from coefficients"""
 
     return len(set(idx for row in coefs for idx, point in enumerate(row) if point))
+
+
+def get_data(
+    use_ccle: bool = True, cachedirpath: Path = Path("cache")
+) -> tuple[np.ndarray, np.ndarray, list[str], list[str]]:
+    """Get X and y data"""
+
+    y_name = "primary_disease"
+    to_remove = ["Unknown", "Teratoma", "Adrenal Cancer", "Embryonal Cancer"]
+    sample_info_csv_filepath = Path("22Q2/sample_info.csv")
+    sample_info_on = "DepMap_ID"
+    if use_ccle:
+        df_csv_filepath = "22Q2/CCLE_expression.csv"
+        df_on = "Unnamed: 0"
+    else:
+        df_csv_filepath = "22Q2/CRISPR_gene_effect.csv"
+        df_on = "DepMap_ID"
+
+    df = pd.read_csv(cachedirpath / df_csv_filepath)
+    sample_info = pd.read_csv(cachedirpath / sample_info_csv_filepath)
+
+    df.dropna(inplace=True)
+    df = df.merge(
+        sample_info[[sample_info_on, y_name]],
+        left_on=df_on,
+        right_on=sample_info_on,
+        how="inner",
+    )
+    for label in to_remove:
+        df = df[df[y_name] != label]
+    labels, y_data = np.unique(df[y_name], return_inverse=True)
+    df = df.select_dtypes(include=[np.number])
+
+    return df.to_numpy(), y_data, labels.tolist(), df.columns.tolist()
 
 
 class BasicResults:
